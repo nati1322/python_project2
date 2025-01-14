2e29cfcd5a43d19c5d9a4e7bb7abb330e1f8e59d
nati1322 <natnaelterefe44@gmail.com>
2025-01-14 16:03:01 +0300

    Modified contact.py

--- contact.py
+++ contact.py
@@ -6,15 +6,19 @@ print('''
 
 lists = []
 while True:
-    items = input("enter your selection: ")
+    items = input("Enter your selection: ")
     if items == "1":
-        for i in lists:
-            print(i)
+        for index, item in enumerate(lists):
+            print(f"{index + 1}. {item}")
     elif items == "2":
-        add_items = input("enter you need to add to the list: ")
+        add_items = input("Enter what you need to add to the list: ")
         lists.append(add_items)
     elif items == "3":
-        remove_items = input("enter you need to remove from the list: ")
-        lists.remove(remove_items)
-    else:
-        print(f"{items} is invalid pleas enter valid selection")
+        try:
+            remove_index = int(input("Enter the index of the item to remove: "))
+            if 0 <= remove_index < len(lists):
+                removed_item = lists.pop(remove_index)
+                print(f"Removed: {removed_item}")
+        except:
+            print("Invalid input. Please enter a number.")
+    
